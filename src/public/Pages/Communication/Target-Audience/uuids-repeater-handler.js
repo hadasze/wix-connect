98731a9956fc7d0c9203e80a55277d99087794cc
itziks <itziks@wix.com>
2023-05-02 12:32:22 +0300

    itzik

--- src/public/Pages/Communication/Target-Audience/uuids-repeater-handler.js
+++ src/public/Pages/Communication/Target-Audience/uuids-repeater-handler.js
@@ -48,6 +48,7 @@ const setTargetAudienceData = () => {
 
     autorun(async () => {
         if (state.communication?.targetAudience) {
+            console.log('setTargetAudienceData::AutoRun');
             await $w('#TargetAudienceContent').changeState('TargetAudienceContentLoading');
             await setAllRepeatersAudienceData();
             await $w('#TargetAudienceContent').changeState('TargetAudienceContentLoaded') && $w('#csvDetailsAndActionsBox').show();
