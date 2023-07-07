8a434bef0cd3885cc93eeb9fa486e504a0ba5dd2
Bmeron <binyaminm@wix.com>
2023-07-07 14:44:03 +0300

    revert login

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,16 +1,21 @@
 import wixSite from 'wix-site';
-import { redirectToMyCommunications } from 'public/_utils.js'
+// import { redirectToMyCommunications } from 'public/_utils.js'
 import { validateAccessToken, clearQueryParams } from '../login.js';
-
+import wixLocation from 'wix-location';
 // redirect from blank home page
-if (wixSite.currentPage.isHomePage) {
-    redirectToMyCommunications();
-} else {
+// if (wixSite.currentPage.isHomePage) {
+//     redirectToMyCommunications();
+// } else {
+    
+// redirect from blank home page
+if (wixSite.currentPage.isHomePage)
+    wixLocation.to(wixLocation.baseUrl + '/my-communications');
+
     $w.onReady(function () {
         clearQueryParams();
         refreshTokenTimeout();
     });
-}
+// }
 
 let tokenExpire;
 
