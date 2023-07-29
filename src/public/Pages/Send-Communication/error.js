794dde34df9afa1f348a1a1c7a3fce6b3008a514
Bmeron <binyaminm@wix.com>
2023-07-29 22:19:17 +0300

    fix gotiton click

--- src/public/Pages/Send-Communication/error.js
+++ src/public/Pages/Send-Communication/error.js
@@ -2,8 +2,8 @@ import { SendCommunication as Comp } from '../../components.js';
 import { redirectToMyCommunications } from '../../_utils.js';
 
 export function setEvents() {
-    Comp.gotItButton.onClick((event) => {
-        Comp.gotItButton.disable();
+    Comp.gotItErrorButton.onClick((event) => {
+        Comp.gotItErrorButton.disable();
         redirectToMyCommunications();
     })
 }
