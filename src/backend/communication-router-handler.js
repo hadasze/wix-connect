46ecc3e6482bd2d3f78923385f04f2fe90c05d6d
Bmeron <binyaminm@wix.com>
2023-08-13 22:00:55 +0300

    use s2s token on dashboard router

--- src/backend/communication-router-handler.js
+++ src/backend/communication-router-handler.js
@@ -1,7 +1,10 @@
 import { ok, sendStatus, redirect } from "wix-router";
 import { getCommunication, getAllUserCommunications } from './data-methods.js';
 import { countAllUserCommunications } from './data-methods.js';
+import { getServerToken } from "./ero-api.js";
+
 import * as MarketingAPI from './marketing-api.js';
+
 
 export async function setCommunication(routerRequest) {
     try {
@@ -26,21 +29,20 @@ export async function setMyCommunications(routerRequest) {
 
     // get all user communications that are not delete
     // split into all,draft,archive,sent,templates
-    
+
     try {
         let communicationDetails = {};
-        if (routerRequest.query?.token) {
-            const filters = { "sent": true };
-            const getAllUserCommunicationsRes = await getAllUserCommunications(filters);
-            const uniqueIds = [...new Set(getAllUserCommunicationsRes.items.map((item) => item._id))];
-            if (uniqueIds.length > 0)
-                communicationDetails = await MarketingAPI.getSentCommunications(uniqueIds, routerRequest.query?.token);
-        }
+        const serverTokenSet = await getServerToken();
+        const filters = { "sent": true };
+        const getAllUserCommunicationsRes = await getAllUserCommunications(filters);
+        const uniqueIds = [...new Set(getAllUserCommunicationsRes.items.map((item) => item._id))];
+        if (uniqueIds.length > 0)
+            communicationDetails = await MarketingAPI.getSentCommunications(uniqueIds, serverTokenSet.access_token);
 
         const [all, draft, archive, sent, templates] = await countAllUserCommunications();
         return ok("my-communications-page", { count: { all, draft, archive, sent, templates }, communicationDetails });
     } catch (error) {
-        return sendStatus('500', 'backend -> comunication-router-handler -> setCommunication failed - origen error - ' + error.message);
+        return sendStatus('500', 'backend -> comunication-router-handler -> setCommunication failed - origin error - ' + error.message);
     }
 }
 
@@ -50,6 +52,6 @@ export async function setPreview(routerRequest) {
         const communication = await getCommunication(communicationID);
         return ok("preview-page", communication);
     } catch (err) {
-        return sendStatus('500', 'backend -> comunication-router-handler -> setPreview failed - origen error - ' + err.message);
+        return sendStatus('500', 'backend -> comunication-router-handler -> setPreview failed - origin error - ' + err.message);
     }
 }
